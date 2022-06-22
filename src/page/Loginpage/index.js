4fec48cbf4d5eee33f5b98d90f66f5e38a2f0150
Nhat Phi <nhatphi2106@gmail.com>
2022-06-22 17:42:16 +0700

    remove something we dont need in Login Page and we need handle it

--- src/page/Loginpage/index.js
+++ src/page/Loginpage/index.js
@@ -1,48 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 import JoinInput from "../../components/JoinInput/JoinInput";
-import socket from "../../connections/socket";
-import { setReduxPlayer } from "../../redux/reducers/playerReducer";
+
 
 import "./index.scss"
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function LoginPage() {
-  const [playerName, setPlayerName] = useState("");
-  const [room, setRoom] = useState("");
 
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
-
-  const _handlePlayerNameInput = (e) => {
-    setPlayerName(e.target.value);
-  };
-
-  const _handleSubmit = () => {
-    socket.emit("joinRoom", { room, name: playerName });
-  };
-
-  const _handleRoomInput = (e) => {
-    setRoom(e.target.value);
-  };
-
-  useEffect(() => {
-    socket.on("joinRoomRes", (res) => {
-      if (res.result) {
-        dispatch(setReduxPlayer(res.player));
-        navigate(
-          `/waiting?room=${res.player.room}&playerName=${res.player.name}`
-        );
-      } else {
-        toast.error("Oop! Maybe the code is wrong!!!");
-
-      }
-    });
-  }, [dispatch,navigate]);
 
   return (
     <div className="homeContainer" >
@@ -50,13 +16,9 @@ export default function LoginPage() {
       <div className="groupButton" >
       <JoinInput
         label={"Email"}
-        value={playerName}
-        onChangeHandle={_handlePlayerNameInput}
       />
       <JoinInput
         label={"Password"}
-        value={room}
-        onChangeHandle={_handleRoomInput}
       />
       <button className="loginButton" >Login</button>
 
