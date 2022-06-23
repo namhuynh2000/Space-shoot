6f97e348aa54772878661adcffe6323153ec00b9
Nhat Phi <nhatphi2106@gmail.com>
2022-06-23 09:16:16 +0700

    new UI for login Page

--- src/page/Loginpage/index.js
+++ src/page/Loginpage/index.js
@@ -1,8 +1,4 @@
 import { Link} from "react-router-dom";
-
-import JoinInput from "../../components/JoinInput/JoinInput";
-
-
 import "./index.scss"
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,21 +7,24 @@ export default function LoginPage() {
 
 
   return (
-    <div className="homeContainer" >
-      <ToastContainer />
-      <div className="groupButton" >
-      <JoinInput
-        label={"Email"}
-      />
-      <JoinInput
-        label={"Password"}
-      />
-      <button className="loginButton" >Login</button>
+    <div className="loginContainer" >
+        <ToastContainer />
+        <div className="loginContainer__background"></div>
+        <div className="loginContainer__form" >
+        <text className="loginContainer__form__text1">Login to your Account</text>
+        <text className="loginContainer__form__text2">with your registered Email Address</text>
+        <hr color="gray"></hr>
+        <label >Email address</label>
+        <input className="loginContainer__form__emailInput" placeholder="Enter email address"></input>
+        <label>Enter Password</label>
+        <input className="loginContainer__form__passwordInput" placeholder="Password"></input>
+        <button className="loginButton" >Login</button>
+        <p className="loginContainer__form__hr">Or</p>
 
-      <button className="googleButton" >Login by Google Account</button>
-    <div>Not a member?    <Link to={"/host"}>Register!</Link></div>
+        <button className="googleButton" >Login with Google</button>
+        <div style={{textAlign:"right"}}>Not a member?    <Link style={{color:"black", fontWeight:"bolder"}} to={"/host"}>Register!</Link></div>
 
-        
+          
       </div>
 
     </div>
