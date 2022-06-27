c48a4c2409d1e90f4894e7f317fb2c1c34bf3494
namhuynh2000 <hqnam.it@gmail.com>
2022-06-28 01:26:55 +0700

    handle login validation

--- src/page/Loginpage/index.js
+++ src/page/Loginpage/index.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./index.scss";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { auth } from "../../fire";
 import { useNavigate } from "react-router-dom";
@@ -35,7 +35,7 @@ export default function LoginPage() {
         navigate("/host");
       })
       .catch((error) => {
-        console.error(error);
+        toast.error("Oop! Something wrong!!!");
       });
   };
 
@@ -56,7 +56,17 @@ export default function LoginPage() {
       // console.log(user);
       navigate("/host");
     } catch (error) {
-      console.error(error.message);
+      if (error.code === 'auth/wrong-password') {
+        toast.error("Password incorrect!");
+      }
+      else if (error.code === 'auth/user-not-found')
+        toast.error("Email does not exist!");
+      else if (error.code === 'auth/too-many-requests')
+        toast.error("Temporarily disabled due to many failed login attempts!");
+      else
+        toast.error("Oop! Something wrong!!!")
+      console.log(error.message);
+
     }
   };
 
@@ -89,7 +99,7 @@ export default function LoginPage() {
               }}
             ></input>
             <Link to={"/forgot"}>
-            <div className="forgot">Forgot password?</div>
+              <div className="forgot">Forgot password?</div>
             </Link>
             <button className="loginButton" onClick={login}>
               Login
