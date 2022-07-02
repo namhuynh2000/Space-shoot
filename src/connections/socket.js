89d6dfcc811478bb5431cb99e1885897c1356d93
ducloc0009 <ducloc0009@gmail.com>
2022-07-02 21:43:56 +0700

    fix bug add choice

--- src/connections/socket.js
+++ src/connections/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-// const ENDPOINT = "https://spaceshoot-server.herokuapp.com";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://spaceshoot-server.herokuapp.com";
+// const ENDPOINT = "http://localhost:5000";
 const socket = io(ENDPOINT, { forceNew: true });
 
 export default socket;
