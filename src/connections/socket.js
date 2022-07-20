1aa4148b75b8e37b247c53b3f942c9110fce30ee
DESKTOP-HS39Q2U\ADMIN <ducloc0009@gmail.com>
2022-07-20 21:55:48 +0700

    add env

--- src/connections/socket.js
+++ src/connections/socket.js
@@ -1,7 +1,6 @@
 import io from "socket.io-client";
 
-// const ENDPOINT = "https://spaceshoot-server.herokuapp.com";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = process.env.REACT_APP_BACK_END_URL || "http://localhost:5000";
 const socket = io(ENDPOINT, { forceNew: true });
 
 export default socket;
